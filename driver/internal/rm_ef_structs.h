b20e8c94c1e879eb4c7df59c91a3aa98105becf8
nmac-dev <ntwmac.dev@gmail.com>
2025-02-02 21:20:35 +0000

    :ADDED: internal/rm_ef_structs.h

---
+++ driver/internal/rm_ef_structs.h
@@ -0,0 +1,13 @@
+/// @file  internal/rm_ef_structs.h
+/// @brief ISM330DHCX Internal Structs for Embedded Advanced Features Page 1 Register Mapping
+
+#ifndef __ISM330DHCX_INTERNAL__RM_EF_STRUCTS_H__
+#define __ISM330DHCX_INTERNAL__RM_EF_STRUCTS_H__
+
+#include "c-nstd/types.h"
+
+
+/// NOTE: Embedded advanced features registers are accessible when PAGE_SEL[3:0] is set to '0001' in n PAGE_SEL (register)
+
+
+#endif /* __ISM330DHCX_INTERNAL__RM_EF_STRUCTS_H__ */
