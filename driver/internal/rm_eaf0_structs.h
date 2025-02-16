329290b289996c13f1b1ef947fac6bf371cf2f4a
nmac-dev <ntwmac.dev@gmail.com>
2025-02-16 22:34:13 +0000

    :ADDED: internal/rm_eaf0_structs.h

---
+++ driver/internal/rm_eaf0_structs.h
@@ -0,0 +1,13 @@
+/// @file  internal/rm_eaf0_structs.h
+/// @brief ISM330DHCX Internal Structs for Embedded Advanced Features (Page 0) Register Mapping
+
+#ifndef __ISM330DHCX_INTERNAL__RM_EAF0_STRUCTS_H__
+#define __ISM330DHCX_INTERNAL__RM_EAF0_STRUCTS_H__
+
+#include "c-nstd/types.h"
+
+
+/// NOTE: Embedded advanced features page 0 registers are accessible when PAGE_SEL[3:0] are set to 0000 in PAGE_SEL
+
+
+#endif /* __ISM330DHCX_INTERNAL__RM_EF_STRUCTS_H__ */
